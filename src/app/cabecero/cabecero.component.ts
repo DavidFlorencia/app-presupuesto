8bf65eac95029d64865084ad63922a163a90ca40
it-dflorenciac <dflorencia.developer@gmail.com>
2025-08-09 17:08:34 -0600

    components

---
+++ src/app/cabecero/cabecero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecero.component',
+  imports: [],
+  templateUrl: './cabecero.component.html',
+  styleUrl: './cabecero.component.css'
+})
+export class CabeceroComponent {
+
+}
---
+++ src/app/egreso/egreso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-egreso.component',
+  imports: [],
+  templateUrl: './egreso.component.html',
+  styleUrl: './egreso.component.css'
+})
+export class EgresoComponent {
+
+}
