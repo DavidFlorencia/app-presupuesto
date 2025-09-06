8ad119a9e505ddbf630113e6cc2f988b9db61718
it-dflorenciac <dflorencia.developer@gmail.com>
2025-09-06 13:23:39 -0600

    add record

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,17 +1,46 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IngresoService } from '../ingreso/ingreso.service';
+import { EgresoService } from '../egreso/egreso.service';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { Egreso } from '../egreso/egreso.model';
 
 @Component({
   selector: 'app-formulario',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
-  tipo: string = 'egresoOperacion';
+  tipo: string = 'ingresoOperacion';
+  descripcionInput: string | null = null;
+  valorInput: number | null = null;
+
+  constructor(private ingresoServicio: IngresoService, private egresoServicio: EgresoService) { 
+
+  }
 
   tipoOperacion(event: Event) {
     const elementoSelect = (event.target as HTMLSelectElement);
     this.tipo = elementoSelect.value;
+  }
+
+  agregarValor() {
+    if (this.descripcionInput !== null && this.valorInput !== null) {
+      if (this.tipo === 'ingresoOperacion') {
+        this.ingresoServicio.ingresos.push(
+          new Ingreso(this.descripcionInput, this.valorInput)
+        )
+      } else {
+        this.egresoServicio.egresos.push(
+          new Egreso(this.descripcionInput, this.valorInput)
+        )
+      }
+    } else {
+      console.log("Faltan datos");
+    }
+    this.descripcionInput = null;
+    this.valorInput = null;
   }
 }
