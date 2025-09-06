920af126fbe4246c7deefebdf1129cedca54934d
it-dflorenciac <dflorencia.developer@gmail.com>
2025-09-06 10:38:59 -0600

    main logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,10 @@ import { CabeceroComponent } from "./cabecero/cabecero.component";
 import { FormularioComponent } from "./formulario/formulario.component";
 import { IngresoComponent } from "./ingreso/ingreso.component";
 import { EgresoComponent } from "./egreso/egreso.component";
+import { Ingreso } from './ingreso/ingreso.model';
+import { Egreso } from './egreso/egreso.model';
+import { IngresoService } from './ingreso/ingreso.service';
+import { EgresoService } from './egreso/egreso.service';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +16,38 @@ import { EgresoComponent } from "./egreso/egreso.component";
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'app-presupuesto';
+  ingresos: Ingreso[] = [];
+  egresos: Egreso[] = [];
+
+  constructor(
+    private ingresoServicio: IngresoService,
+    private egresoServicio: EgresoService
+  ) {
+    this.ingresos = ingresoServicio.ingresos;
+    this.egresos = egresoServicio.egresos;
+  }
+
+  getIngresoTotal(): number {
+    let ingresoTotal: number = 0;
+    this.ingresos.forEach(ingreso => {
+      ingresoTotal += ingreso.valor;
+    });
+    return ingresoTotal;
+  }
+
+  getEgresoTotal(): number {
+    let egresoTotal: number = 0;
+    this.egresos.forEach(egreso => {
+      egresoTotal += egreso.valor;
+    });
+    return egresoTotal;
+  }
+
+  getPorcentajeTotal(): number {
+    return this.getEgresoTotal() / this.getIngresoTotal();
+  }
+
+  getPresupuestoTotal(): number {
+    return this.getIngresoTotal() - this.getEgresoTotal();
+  }
 }
